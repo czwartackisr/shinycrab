4930524856b061826de89165855f2ad7916658b7
Stephen Czwartacki <czwartackisr@g.cofc.edu>
2019-10-24 21:07:01 -0400

    Landings, gathered crab

--- Chapter1/scripts/Ch1_TimeSeries.R
+++ Chapter1/scripts/Ch1_TimeSeries.R
@@ -16,10 +16,10 @@ crab <- read.csv("./Chapter1/data/CH1data.csv", stringsAsFactors = FALSE)
 
 crab = crab %>%
   filter(Year > 1979) %>%
-  select(1:14, 17) 
+  select(1:14, 17, 19) 
 
 B90 = crab %>%
-  select(1:7) %>%
+  select(1:7)%>%
   rename(Total = B90_CPUE,
          Juvenile = B90_JuvCPUE,
          Subadult = B90_SubadultCPUE,
@@ -30,7 +30,8 @@ B90 = crab %>%
   mutate(ProjID = "Harbor Trawl") %>%
   select(1, 4, 2, 3)
 
-T38 = select(crab, 1, 8:13)%>%
+T38 = crab %>% 
+  select(1, 8:13) %>%
   rename(Total = T38_CPUE,
          Juvenile = T38_JuvCPUE,
          Subadult = T38_SubadultCPUE,
@@ -41,28 +42,32 @@ T38 = select(crab, 1, 8:13)%>%
   mutate(ProjID = "Harbor Trawl") %>%
   select(1, 4, 2, 3)
 
-T06 = select(crab, 1, 14) %>%
+T06 = crab %>%
+  select(1, 14) %>%
   filter(Year > 2005) %>%
   rename(Total = T06_CPUE) %>%
   gather(Lifestage, CPUE, 2) %>%
-  mutate(ProjID = "Total") %>%
+  mutate(ProjID = "Trammel Net") %>%
   select(1, 4, 2, 3)
 
 
-LandingsCPUE = select(crab, 1, 15) %>%
-  filter(Year > 2003) %>%
-#######STOPPED HERE#######  
+Landings = crab %>%
+  select(1, 15, 16) %>%
+  rename(LandingsCPUE = MeanLandingsCPUE) %>%
+  gather(ProjID, CPUE, 2:3) %>%
+  filter(CPUE != "") %>%
+  mutate(Lifestage = "Legal") %>%
+  select(1, 2, 4, 3)
   
 
-B90 %>%
-  rename(Total = B90_CPUE,
-         Juvenile = B90_JuvCPUE,
-         Subadult = B90_SubadultCPUE,
-         Adult = B90_AdultCPUE,
-         "Immature Female" = B90_ImmatureFemaleCPUE,
-         "Mature Female" = B90_MatureFemaleCPUE)
 
-crab2 = cbind(B90, T38, T06, )
+crab2 = rbind(B90, T38, T06, Landings)
+
+ggplot(aes(Year, CPUE), data = B90) +
+  ggtitle("Harbor Trawl Annual Abundance") +
+  geom_point()
+  
+
 
 
 Fig_plot<-plot_grid(T38_YSI, T38_Raw, T38_CSI, nrow = 1)
